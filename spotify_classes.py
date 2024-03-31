89d1e2b9f10bce509e355ee4cfc0893609425540
ilyazinger <ilya.zinger@gmail.com>
2024-03-30 23:42:38 -0400

    init of oop strucutre

--- spotify_classes.py
+++ spotify_classes.py
@@ -1,5 +1,18 @@
+import requests
+from get_token import get_token
 
+ALBUM_ENDPOINT = 'https://api.spotify.com/v1/albums/'
 
 class Album():
-    def __init__(self, album):
-        self.album = album
+    def __init__(self, name, artists, tracklist):
+        self.name = name
+        self.artists = artists
+        self.tracklist = tracklist
+    
+
+def get_album():
+    return Album(name, artists, tracklist)
+
+    return requests.get(ALBUM_ENDPOINT+album_id, headers=headers)
+        
+
